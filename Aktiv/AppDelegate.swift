a1e892086e4bf5c3e4f6cde581f809f800e50613
Hugo Cardante <hugocardante@gmail.com>
2024-02-18 17:30:43 +0000

    General bugs and improvement of main logic

--- Aktiv/AppDelegate.swift
+++ Aktiv/AppDelegate.swift
@@ -10,90 +10,126 @@ import IOKit.ps
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
     var statusBar: NSStatusBar!
-    var statusBarItem: NSStatusItem!
-    var batteryStatusTimer: Timer?
-    var screenOnTimeSinceLastUnplugged: TimeInterval = 0
-    var totalUptimeSinceAppStarted: TimeInterval = 0
-    var appStartTime: Date?
-    var activeDisplayMenuItem: NSMenuItem!
-    var totalUptimeMenuItem: NSMenuItem!
-    var lastUpdateTime: Date?
-    var isSleeping: Bool = false
-    var isCharging: Bool = false
+        var statusBarItem: NSStatusItem!
+        var batteryStatusTimer: Timer?
+        var screenOnTimeSinceLastUnplugged: TimeInterval = 0
+        var totalUptimeSinceAppStarted: TimeInterval = 0
+        var appStartTime: Date?
+        var activeDisplayMenuItem: NSMenuItem!
+        var totalUptimeMenuItem: NSMenuItem!
+        var lastUpdateTime: Date?
+        var isSleeping: Bool = false
+        var wasCharging: Bool = false
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        statusBar = NSStatusBar.system
-        statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
-        statusBarItem.button?.title = "Calculating..."
-        
-        appStartTime = Date()
-        lastUpdateTime = appStartTime // Initialize the last update time
-        
-        setupMenu()
-        registerForNotifications()
-        startTimer()
-        loadState() // Load saved state, if any
-    }
-
-    func setupMenu() {
-        let menu = NSMenu()
-
-        activeDisplayMenuItem = NSMenuItem(title: "Screen On Time: Calculating...", action: nil, keyEquivalent: "")
-        totalUptimeMenuItem = NSMenuItem(title: "Total Uptime: Calculating...", action: nil, keyEquivalent: "")
-        let creatorMenuItem = NSMenuItem(title: "Created by: Hugo Cardante", action: nil, keyEquivalent: "")
-        creatorMenuItem.isEnabled = false // Make non-selectable
-        let quitMenuItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
-        
-        menu.addItem(activeDisplayMenuItem)
-        menu.addItem(totalUptimeMenuItem)
-        menu.addItem(creatorMenuItem)
-        menu.addItem(NSMenuItem.separator())
-        menu.addItem(quitMenuItem)
-
-        statusBarItem.menu = menu
-    }
-
-    func registerForNotifications() {
-        let notificationCenter = NSWorkspace.shared.notificationCenter
-        notificationCenter.addObserver(self, selector: #selector(systemDidWake), name: NSWorkspace.didWakeNotification, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(systemWillSleep), name: NSWorkspace.willSleepNotification, object: nil)
-    }
-
-    func startTimer() {
-        batteryStatusTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
-            self?.updateTimeTracking()
-        }
-    }
-
-    @objc func updateTimeTracking() {
-        guard let lastUpdate = lastUpdateTime else { return }
-        let now = Date()
-        let elapsedTime = now.timeIntervalSince(lastUpdate)
-        lastUpdateTime = now
-
-        // Update total uptime regardless of sleeping or charging status
-        totalUptimeSinceAppStarted += elapsedTime
-
-        // Only update screen on time if not sleeping and not charging
-        if !isSleeping && !isCharging {
-            screenOnTimeSinceLastUnplugged += elapsedTime
+        func applicationDidFinishLaunching(_ aNotification: Notification) {
+            statusBar = NSStatusBar.system
+            statusBarItem = statusBar.statusItem(withLength: NSStatusItem.variableLength)
+            statusBarItem.button?.title = "Calculating..."
+            
+            appStartTime = Date()
+            lastUpdateTime = appStartTime // Initialize the last update time
+            
+            setupMenu()
+            registerForNotifications()
+            startTimer()
+            loadState() // Load saved state, if any
         }
 
-        DispatchQueue.main.async { [weak self] in
-            self?.updateUI()
+        func setupMenu() {
+            let menu = NSMenu()
+
+            // Initialize menu items
+            activeDisplayMenuItem = NSMenuItem(title: "Screen On Time: Calculating...", action: nil, keyEquivalent: "")
+            totalUptimeMenuItem = NSMenuItem(title: "Total Uptime: Calculating...", action: nil, keyEquivalent: "")
+            let creatorMenuItem = NSMenuItem(title: "Created by: Hugo Cardante", action: nil, keyEquivalent: "")
+            creatorMenuItem.isEnabled = false
+            let quitMenuItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "q")
+
+            // Add items to menu
+            menu.addItem(activeDisplayMenuItem)
+            menu.addItem(totalUptimeMenuItem)
+            menu.addItem(creatorMenuItem)
+            menu.addItem(NSMenuItem.separator())
+            menu.addItem(quitMenuItem)
+
+            statusBarItem.menu = menu
         }
-    }
 
-    func updateUI() {
-        let screenOnHours = Int(screenOnTimeSinceLastUnplugged) / 3600
-        let screenOnMinutes = (Int(screenOnTimeSinceLastUnplugged) % 3600) / 60
-        statusBarItem.button?.title = "Aktiv: \(screenOnHours)h \(screenOnMinutes)m"
-        activeDisplayMenuItem.title = "Screen On: \(screenOnHours)h \(screenOnMinutes)m"
+        func registerForNotifications() {
+            let notificationCenter = NSWorkspace.shared.notificationCenter
+            notificationCenter.addObserver(self, selector: #selector(systemDidWake), name: NSWorkspace.didWakeNotification, object: nil)
+            notificationCenter.addObserver(self, selector: #selector(systemWillSleep), name: NSWorkspace.willSleepNotification, object: nil)
+        }
 
-        let uptimeHours = Int(totalUptimeSinceAppStarted) / 3600
-        let uptimeMinutes = (Int(totalUptimeSinceAppStarted) % 3600) / 60
-        totalUptimeMenuItem.title = "Total Uptime: \(uptimeHours)h \(uptimeMinutes)m"
-    }
+        func startTimer() {
+            batteryStatusTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] _ in
+                self?.updateTimeTracking()
+            }
+        }
+
+        @objc func updateTimeTracking() {
+            let isCurrentlyCharging = self.isCurrentlyCharging()
+            let now = Date()
+            guard let lastUpdate = lastUpdateTime else {
+                lastUpdateTime = now
+                return
+            }
+            
+            let elapsedTime = now.timeIntervalSince(lastUpdate)
+            lastUpdateTime = now
+
+            if isCurrentlyCharging != wasCharging {
+                // If charging state has changed, reset the timers
+                if isCurrentlyCharging {
+                    screenOnTimeSinceLastUnplugged = 0
+                    totalUptimeSinceAppStarted = 0
+                }
+                wasCharging = isCurrentlyCharging
+            } else if !isSleeping && !isCurrentlyCharging {
+                // Update screen on time if not sleeping and not charging
+                screenOnTimeSinceLastUnplugged += elapsedTime
+            }
+
+            // Always update total uptime
+            totalUptimeSinceAppStarted += elapsedTime
+
+            DispatchQueue.main.async { [weak self] in
+                self?.updateUI(isCharging: isCurrentlyCharging)
+            }
+        }
+
+        func isCurrentlyCharging() -> Bool {
+            guard let powerSourceInfo = IOPSCopyPowerSourcesInfo()?.takeRetainedValue(),
+                  let powerSourcesList = IOPSCopyPowerSourcesList(powerSourceInfo)?.takeRetainedValue() as NSArray?,
+                  let powerSource = powerSourcesList.firstObject else {
+                return false
+            }
+            
+            if let powerSourceDescription = IOPSGetPowerSourceDescription(powerSourceInfo, powerSource as CFTypeRef)?.takeUnretainedValue() as NSDictionary?,
+               let isCharging = powerSourceDescription[kIOPSIsChargingKey] as? Bool {
+                return isCharging
+            }
+            
+            return false
+        }
+
+        func updateUI(isCharging: Bool) {
+            if isCharging {
+                statusBarItem.button?.title = "Charging..."
+            } else {
+                // Update screen on time display
+                let screenOnHours = Int(screenOnTimeSinceLastUnplugged) / 3600
+                let screenOnMinutes = (Int(screenOnTimeSinceLastUnplugged) % 3600) / 60
+                statusBarItem.button?.title = "Aktiv: \(screenOnHours)h \(screenOnMinutes)m"
+                activeDisplayMenuItem.title = "Screen On: \(screenOnHours)h \(screenOnMinutes)m"
+
+                // Update total uptime display
+                let uptimeHours = Int(totalUptimeSinceAppStarted) / 3600
+                let uptimeMinutes = (Int(totalUptimeSinceAppStarted) % 3600) / 60
+                totalUptimeMenuItem.title = "Total Uptime: \(uptimeHours)h \(uptimeMinutes)m"
+            }
+        }
+
 
     @objc func systemWillSleep(notification: Notification) {
         isSleeping = true
